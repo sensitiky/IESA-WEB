dd641d3075da4ce36b14e70a532867d0d02a767b
sensitiky <tikybenitez@gmail.com>
2024-07-12 09:28:10 -0300

    Correciones

--- src/app/instituto/page.tsx
+++ src/app/instituto/page.tsx
@@ -46,7 +46,7 @@ export default function Instituto() {
         <Carousel />
         <div className="space-y-6">
           <div>
-            <h2 className="text-3xl font-bold">
+            <h2 className="text-3xl font-bold text-center">
               Instituto de Estudios Superiores Argentino
             </h2>
             <p className="mt-2 text-gray-500 dark:text-gray-400 text-center">
